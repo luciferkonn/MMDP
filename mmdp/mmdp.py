bd9382f38f6176563cf39ba1d9404ef6f827d4f5
luciferkonn <luciferkonn@gmail.com>
2018-05-09 15:46:21 -0400

    multi-agent

--- mmdp/mmdp.py
+++ mmdp/mmdp.py
@@ -11,6 +11,7 @@ ROOT = '/home/lucifer/Documents/Git/MMDP/'
 def mqlearning(args, env, episode_len=1000, learning_rate=0.9, epsilon=0.1, gamma=1, runs=30):
     grid_size = args.grid_size
     n_actions = args.n_actions
+    n_agents = args.n_agents
 
     # total reward
     total_reward = np.zeros(episode_len)
@@ -18,13 +19,14 @@ def mqlearning(args, env, episode_len=1000, learning_rate=0.9, epsilon=0.1, gamm
     for j in range(runs):
         print('RUNS:{}'.format(j))
         # initialization
-        q_value = np.zeros((grid_size*grid_size, n_actions))
+        q_value = np.zeros((grid_size*grid_size, n_actions, n_actions**(n_agents-1)))
         # loop for each episode
         for i in range(episode_len):
             # initialize each agent's state
             states, done = env.reset()
             total = 0
             action = [0] * len(states)
+            action_others = [0] * len(action)
             # loop for every state of agent
             while not done[0]:
                 for j, state in enumerate(states):
@@ -35,17 +37,29 @@ def mqlearning(args, env, episode_len=1000, learning_rate=0.9, epsilon=0.1, gamm
                     if np.random.binomial(1, epsilon) == 1:
                         action[j] = np.random.randint(n_actions)
                     else:
-                        action[j] = np.argmax(q_value[x*grid_size+y])
+                        action[j] = np.argmax(q_value[x*grid_size+y,:,action_others[j]])
+                        # print(action[j])
                 # every agent take a step
                 next_state, reward, done, info = env.step(action)
-                # print(reward)
+
+                # find the others actions
+                for k, _ in enumerate(action):
+                    # implement agent others
+                    action_oth = np.delete(action, k, 0)
+                    action_others[k] = 0
+                    for oth, _ in enumerate(action_oth):
+                        if oth == len(action_oth) - 1:
+                            action_others[k] += action_oth[oth]
+                        else:
+                            action_others[k] += action_oth[oth] * n_actions
 
                 # updata q-value simultaneously
                 for k, _ in enumerate(action):
                     # print(info['n'][k]['event'])
                     (next_x, next_y) = next_state[k]['loc']
-                    q_value[x*grid_size+y, action[k]] += learning_rate * (reward[k] + gamma * np.max(q_value[next_x *
-                                                            grid_size + next_y, :]) - q_value[x*grid_size+y, action[k]])
+
+                    q_value[x*grid_size+y, action[k], action_others[k]] += learning_rate * (reward[k] + gamma * np.max(q_value[next_x *
+                                                            grid_size + next_y, :, action_others[k]]) - q_value[x*grid_size+y, action[k], action_others[k]])
                     (x, y) = (next_x, next_y)
                     total += reward[k]
                 states = env.get_states()
@@ -80,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('--grid_size', default=6, type=int, help='the size of a grid world')
     parser.add_argument('--n_actions', default=5, type=int, help='total number of actions an agent can take')
     parser.add_argument('--filename', default=ROOT+'/data/pr.txt', type=str, help='Pick-up probability file')
-    parser.add_argument('--n_agents', default=1, type=int, help='the number of agent play in the environment')
+    parser.add_argument('--n_agents', default=2, type=int, help='the number of agent play in the environment')
     parser.add_argument('--runs', default=1, type=int, help='the number of times run the game')
 
     # parser args
